5441f0ec999735b3ead1c517a6dcf1d7784690b6
DeRDyR <agerlev33@gmail.com>
2025-05-18 20:00:14 +0300

    update MainScreen and create class FirstScreen

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ from kivy.clock import Clock
 import random
 import time
 
+class FirstScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        
+
+
+
+
+
 class MainScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -18,15 +28,24 @@ class MainScreen(Screen):
         second_layout = BoxLayout()
 
         text = TextInput(text="Переведите слово", font_size=25, size_hint=(0.6, 0.4))
-        word = Label(text=":..:.::.:'", font_size=50)
+        word = Label(text="⠏⠗⠊⠺⠑⠞", font_size=50)
+        check = Button(text='Проверить')
 
         first_layout.add_widget(word)
         second_layout.add_widget(text)
+        second_layout.add_widget(check)
 
         layout.add_widget(first_layout)
         layout.add_widget(second_layout)
 
         self.add_widget(layout)
+
+        text = text.text
+
+        
+
+
+        
 
 
 class MyApp(App):
